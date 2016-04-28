b2dd16ac308ee5486103582f6fdc585d119041a9
Vincent Le Guevel <eleve@debian>
2016-04-28 15:11:19 +0200

    Module de consultation des frais terminé

--- src/AL/GsbBundle/Controller/AccueilController.php
+++ src/AL/GsbBundle/Controller/AccueilController.php
@@ -8,10 +8,14 @@ class AccueilController extends Controller
 {
     public function indexAction()
     {
-        if(! isset($_SESSION["visiteur"])) {
+        if(isset($_SESSION["visiteur"])) {         
+            return $this->render("ALGsbBundle:Accueil:accueil.html.twig", array('visiteur'=>$_SESSION['visiteur']));
+        }else if(isset($_SESSION["comptable"])){
+            return $this->render("ALGsbBundle:Accueil:accueil.html.twig", array('comptable'=>$_SESSION['comptable']));
+        }else{
             return $this->redirectToRoute('al_gsb_connexion'); 
-        }       
-            return $this->render("ALGsbBundle:Accueil:accueil.html.twig", array('visiteur'=>$_SESSION['visiteur']));           
+        }  
+                       
           
     }
 }
